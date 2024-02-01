f8de2ace03fccf6b2da12ac986efb9d0add047ec
antonio <toninhoport@gmail.com>
2024-02-01 17:42:36 -0300

    Feat: add new node view engine <EJS> to build a extension

--- index.js
+++ index.js
@@ -3,74 +3,113 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const fsPromieses = require("fs/promises");
 const https = require("https");
+const http = require("http");
+const ejs = require("ejs");
+
+/*
 
 
+*/
+
+const template = fs.readFileSync("./views/index.ejs", "utf8");
+const compiledTemplate = ejs.compile(template);
+
+const server = http.createServer((req, res) => {
+  const route = req.url;
+
+  switch (route) {
+    case "/": {
+      // Set the content type to HTML
+      res.setHeader("Content-Type", "text/html");
+
+      // Render the template with data
+      const renderedTemplate = compiledTemplate({
+        title: "Hello EJS",
+        message: "Welcome to EJS with Node.js!",
+      });
+
+      // Send the rendered template as the response
+      res.end(renderedTemplate);
+    }
+    case "/getData": {
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body = chunk.toString();
+      });
+
+      req.on("end", () => {
+        const requestData = JSON.parse(body);
+
+        const url = requestData.dataInput;
+        const path = `${requestData.dirInput}`;
+ 
+        console.log(path)
+        const pathExists = fs.existsSync(path);
+
+        if (!pathExists) fs.mkdirSync(path);
+
+        async function fetchData() {
+          const response = await axios.get(url);
+          return response.data;
+        }
+
+        async function extractImages() {
+          const html = await fetchData();
+          const $ = cheerio.load(html);
+
+          const imageUrls = [];
+
+          $("a.fileThumb").each((index, element) => {
+            imageUrls.push("https://" + element.attribs.href.slice(2));
+          });
+
+          return imageUrls;
+        }
+
+        extractImages()
+          .then((imageUrls) => {
+            imageUrls.forEach((element) => {
+              download(element, path);
+            });
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+
+        const download = (imageUrl, destinationPath) => {
+          https
+            .get(imageUrl, (response) => {
+              const contentType = response.headers["content-type"];
+
+              if (fs.existsSync(path)) {
+                const fileStream = fs.createWriteStream(
+                  destinationPath + "/" + 
+                    Math.floor(Math.random() * 23123120 + 12301239103) +
+                    "." +
+                    contentType.split("/")[1]
+                );
+                response.pipe(fileStream);
+
+                fileStream.on("finish", () => {
+                  fileStream.close();
+                });
+              }
+            })
+            .on("error", (error) => {
+              console.error("Error downloading image:", error);
+            });
+        };
+      });
 
 
+      res.end(); // Complete the response
 
+      break; // Don't forget to break out of the switch statement
+    }
+  }
+});
 
-const url = "https://boards.4chan.org/gif/thread/26640534";
-const folderName = "BOWC";
-
-
-
-
-
-
-const path = `/home/slicky/Documentos/projects/scraper/${folderName}/`;
-
-
-const pathExists = fs.existsSync(path)
-
-if(!pathExists) fs.mkdirSync(path)
-
-async function fetchData() {
-  const response = await axios.get(url);
-  return response.data;
-}
-
-async function extractImages() {
-  const html = await fetchData();
-  const $ = cheerio.load(html);
-
-  const imageUrls = [];
-
-  $("a.fileThumb").each((index, element) => {
-    imageUrls.push("https://" + element.attribs.href.slice(2));
-  });
-
-  return imageUrls;
-}
-
-extractImages()
-  .then((imageUrls) => {
-    imageUrls.forEach((element) => {
-      download(element, path);
-    });
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
-
-const download = (imageUrl, destinationPath) => {
-  https
-    .get(imageUrl, (response) => {
-
-        const contentType = response.headers['content-type'];
-     
-      if (fs.existsSync(path)) {
-        const fileStream = fs.createWriteStream(
-          destinationPath + Math.floor(Math.random() * 23123120 + 12301239103) + "." + contentType.split("/")[1]
-        );
-        response.pipe(fileStream);
-
-        fileStream.on("finish", () => {
-          fileStream.close();
-        });
-      }
-     
-    })
-    .on("error", (error) => {
-      console.error("Error downloading image:", error);
-    });
-};
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
